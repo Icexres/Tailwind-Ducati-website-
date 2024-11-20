f22811bc1cbd035db9d2d288815b4547d540d717
Icexres <160037346+Icexres@users.noreply.github.com>
2024-11-20 22:56:40 +0545

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      borderWidth:{
+        "6":"6px",
+      },
+      width:{
+        "575":"575px",
+      }
+    },
+  },
+  plugins: [],
+}
+
